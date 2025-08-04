13102cc8a1e5414f979d2b150c0152f8d3638618
blancafr <blanca.floresr@alumnos.upm.es>
2025-08-04 21:04:50 +0200

    Fix json format in patient

--- app/schemas/patient.py
+++ app/schemas/patient.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class PatientCreate(BaseModel):
-    gender: str
-    age: str
-    disease: str
-    comment: str
-    date: str
+    gender: str = Field(..., alias="sexo")
+    age: str = Field(..., alias="edad")
+    disease: str = Field(..., alias="enfermedad")
+    comment: str = Field(..., alias="comentario")
+    date: str = Field(..., alias="fecha")
